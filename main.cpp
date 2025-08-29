8c153b2bba1cf1eaf566155558d393478942b8bf
MegamindDaInevitable <edwinreitu@gmail.com>
2025-08-29 11:41:46 +0200

    Adding more code

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QPalette>
+#include <QColor>
 
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(int argc, char argv[]);
+
+    app.setStyle("Fusion");
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(240, 240, 240));
+    palette.setColor(QPalette::WindowText, Qt::black);
     MainWindow w;
     w.show();
     return a.exec();
