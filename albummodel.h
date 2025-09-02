77b6ecba38ca20748d6ac7cd2c80d2d678a19d08
MegamindDaInevitable <edwinreitu@gmail.com>
2025-09-02 16:06:16 +0200

    Adding more code

--- albummodel.h
+++ albummodel.h
@@ -17,6 +17,10 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVarient data(const QModelIndex &index, int role = Qt::DisplayRole) const overrride;
+
+    QVarient headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) overrride;
 };
 
 #endif // ALBUMMODEL_H
