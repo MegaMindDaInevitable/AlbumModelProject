54cb5264723f33c2ae274095264ed784aaa53652
MegamindDaInevitable <edwinreitu@gmail.com>
2025-09-02 16:02:30 +0200

    Adding more code

--- albummodel.h
+++ albummodel.h
@@ -5,12 +5,18 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QAbstractTableModel>
+#include <QList>
 
 
 class AlbumModel
 {
 public:
-    AlbumModel();
+    explicit AlbumModel(QObject *parent = nullptr);
+    //requred overrrides
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVarient data(const QModelIndex &index, int role = Qt::DisplayRole) const overrride;
 };
 
 #endif // ALBUMMODEL_H
