127202f2946af2cc5db7083a8839e75efa992605
MegamindDaInevitable <edwinreitu@gmail.com>
2025-09-09 15:32:06 +0200

    Adding more code

--- albummodel.cpp
+++ albummodel.cpp
@@ -1,8 +1,13 @@
 
 #include "albummodel.h"
+#include <QString>
 
-AlbumModel::AlbumModel()
+AlbumModel::AlbumModel(QObject *parent): QAbstractTableModel(parent)
 {
 
 }
 
+int AlbumModel::rowCount(const QModelIndex &parent) const{
+    Q_UNUSED(parent);
+    return m_albums.size();
+}
